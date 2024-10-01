49a8e1d93950c9393a2a2ff22d224b6ff0bbf2e4
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-10-01 17:47:00 +0100

    test

--- utils/testDB.js
+++
@@ -1,9 +0,0 @@
-// testDB.js
-
-const dbClient = require('./utils/db');
-
-(async () => {
-  console.log('Is MongoDB alive:', dbClient.isAlive());
-  console.log('Number of users:', await dbClient.nbUsers());
-  console.log('Number of files:', await dbClient.nbFiles());
-})();
